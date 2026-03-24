406d217a82016b131531106c9e2c31d6c49ec32c
Njini Favour <njinifavourbemsimbom@gmail.com>
2026-03-24 09:27:23 +0100

    added a .env file for security

--- project/database/connection.php
+++ project/database/connection.php
@@ -1,19 +1,15 @@
 <?php
-$username = "root";
-$password = "njini000";
-$host="localhost";
-$dbname="BA2A_PHP";
+// Simple .env loader
+$env = parse_ini_file(__DIR__ . '/../.env');
 
-// $conn = new mysqli($host, $username, $password, $dbname);
-
-// if($conn->connect_error){
-//     die("Connection failed: " . $conn->connect_error);
-// }
+$username = $env['DB_USER'] ?? "root";
+$password = $env['DB_PASS'] ?? "";
+$host     = $env['DB_HOST'] ?? "localhost";
+$dbname   = $env['DB_NAME'] ?? "";
 
 $conn = mysqli_connect($host, $username, $password, $dbname);
-if(!$conn){
-    die("connection failed");
-}
-// Removed debug echo to prevent header errors
 
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
